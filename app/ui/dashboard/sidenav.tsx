c5c99f988828f1b08a1893a55d6119bd57be3762
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2024-03-20 03:42:53 +0800

    trial

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -5,7 +5,10 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 export default function SideNav() {
   return (
     <div className="flex h-screen flex-col w-48 bg-[#091928] p-2.5 text-white" >
-      <NavLinks/>
+      <div className="flex items-center mb-6 p-1.5 space-x-2" style={{ marginTop: '7px' }}>
+        <Image src="/clerkie.svg" alt="Clerkie Challenge" width={15} height={15} />
+        <h1 className="text-xs font-bold">Clerkie Challenge</h1>
+      </div>
     </div>
   );
 }
