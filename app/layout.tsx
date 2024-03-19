5bd8cf7e30369c9cd0d474a344cd1d286b2214a6
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2024-03-20 03:48:06 +0800

    again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import SideNav from '@/app/ui/dashboard/sidenav';
+import Image from 'next/image';
+import NavLinks from '@/app/ui/dashboard/nav-links';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,13 @@ export default function RootLayout({
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-48">
-        <SideNav />
+      <div className="flex h-screen flex-col w-48 bg-[#091928] p-2.5 text-white" >
+      <div className="flex items-center mb-6 p-1.5 space-x-2" style={{ marginTop: '7px' }}>
+        <Image src="/clerkie.svg" alt="Clerkie Challenge" width={15} height={15} />
+        <h1 className="text-xs font-bold">Clerkie Challenge</h1>
+      </div>
+      <NavLinks/>
+    </div>
       </div>
       <div className="flex-grow md:overflow-y-auto">{children}</div>
     </div>
