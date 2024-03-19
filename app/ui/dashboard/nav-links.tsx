2531f4b8558fe234eaec7c0dff63b48db5139a3f
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2024-03-20 03:24:24 +0800

    try userouter

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,6 +1,4 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -10,26 +8,26 @@ const links = [
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname();
+  const router = useRouter();
 
   return (
     <div className="flex flex-col">
       {links.map((link) => {
-        const isActive = pathname === link.href;
+        const isActive = router.pathname === link.href;
         let className = 'flex h-[32px] items-center justify-start gap-2 rounded-md p-2 text-xs font-medium transition-colors w-full';
 
         if (isActive) {
           className += ' bg-[#424242] text-white';
         } else {
-          className += ' text-gray-300 hover:bg-[#2C3E50] hover:text-white';
+          className += ' bg-[#091928] text-white';
         }
 
         return (
-          <Link key={link.name} href={link.href} legacyBehavior>
-            <div className={className}>
-              <Image src={link.icon} alt={`${link.name} icon`} width={18} height={18} priority />
+          <Link key={link.name} href={link.href} passHref>
+            <a className={className}>
+              <Image src={link.icon} alt={`${link.name} icon`} width={18} height={18} />
               <span>{link.name}</span>
-            </div>
+            </a>
           </Link>
         );
       })}
