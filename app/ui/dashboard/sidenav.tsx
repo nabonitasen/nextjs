f3d7cc5a822401c4410ffc3907ffbf83ddd16420
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2024-03-20 03:03:52 +0800

    change image

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -6,7 +6,7 @@ export default function SideNav() {
   return (
     <div className="flex h-screen flex-col w-48 bg-[#091928] p-2.5 text-white" >
       <div className="flex items-center mb-6 p-1.5 space-x-2" style={{ marginTop: '7px' }}>
-        <Image src="/company.svg" alt="Clerkie Challenge" width={15} height={15} />
+        <Image src="/clerkie.svg" alt="Clerkie Challenge" width={15} height={15} />
         <h1 className="text-xs font-bold">Clerkie Challenge</h1>
       </div>
       <NavLinks/>
