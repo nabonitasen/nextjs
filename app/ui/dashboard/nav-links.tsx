9e137ecc11d9d627ac1251c4d659af1f475f26d8
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2024-03-20 02:58:27 +0800

    homepage change

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const links = [
-  { name: 'Home', href: '/', icon: '/icons/home.png' }, 
-  { name: 'Friends', href: '/friends', icon: '/icons/friends.png' },
+  { name: 'Home', href: '/', icon: '/home.svg' }, 
+  { name: 'Friends', href: '/friends', icon: '/friends.svg' },
 ];
 
 export default function NavLinks() {
