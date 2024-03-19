bc31338f04e8c4c148183190f9598ec072408035
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2024-03-20 02:55:21 +0800

    change

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -17,18 +16,17 @@ export default function NavLinks() {
     <div className="flex flex-col">
       {links.map((link) => {
         const isActive = pathname === link.href;
+        let className = 'flex h-[32px] items-center justify-start gap-2 rounded-md p-2 text-xs font-medium transition-colors w-full';
+
+        if (isActive) {
+          className += ' bg-[424242] text-white';
+        } else {
+          className += ' bg-[#091928] text-white';
+        }
 
         return (
           <Link key={link.name} href={link.href}>
-            <div
-              className={clsx(
-                'flex h-[32px] items-center justify-start gap-2 rounded-md p-2 text-xs font-medium transition-colors w-full',
-                {
-                  'bg-[424242] text-white': isActive,
-                  'bg-[#091928] text-white': !isActive, 
-                }
-              )}
-            >
+            <div className={className}>
               <Image src={link.icon} alt={`${link.name} icon`} width={18} height={18} />
               <span>{link.name}</span>
             </div>
