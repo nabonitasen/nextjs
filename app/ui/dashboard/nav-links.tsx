3c1c7379b136756e413c4c2cd978bc5bec8d4fd0
Nabonita Sen <nabonita.sonali.sen@gmail.com>
2024-03-20 03:40:59 +0800

    another

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,42 +1,35 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 
 const links = [
-  { name: 'Home', href: '/', icon: '/home.svg' },
+  { name: 'Home', href: '/', icon: '/home.svg' }, 
   { name: 'Friends', href: '/friends', icon: '/friends.svg' },
 ];
 
 export default function NavLinks() {
-  const [currentPath, setCurrentPath] = useState('');
-
-  useEffect(() => {
-    // Set the current path based on the window's location
-    if (typeof window !== 'undefined') {
-      setCurrentPath(window.location.pathname);
-    }
-  }, []);
+  const pathname = usePathname();
 
   return (
     <div className="flex flex-col">
       {links.map((link) => {
-        const isActive = currentPath === link.href;
+        const isActive = pathname === link.href;
         let className = 'flex h-[32px] items-center justify-start gap-2 rounded-md p-2 text-xs font-medium transition-colors w-full';
 
         if (isActive) {
-          className += ' bg-[#424242] text-white';
+          className += ' bg-[424242] text-white';
         } else {
-          className += ' text-gray-300 hover:bg-[#2C3E50] hover:text-white';
+          className += ' bg-[#091928] text-white';
         }
 
         return (
-          <Link key={link.name} href={link.href} legacyBehavior>
-            <a className={className}>
+          <Link key={link.name} href={link.href}>
+            <div className={className}>
               <Image src={link.icon} alt={`${link.name} icon`} width={18} height={18} />
               <span>{link.name}</span>
-            </a>
+            </div>
           </Link>
         );
       })}
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -5,10 +5,6 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 export default function SideNav() {
   return (
     <div className="flex h-screen flex-col w-48 bg-[#091928] p-2.5 text-white" >
-      <div className="flex items-center mb-6 p-1.5 space-x-2" style={{ marginTop: '7px' }}>
-        <Image src="/clerkie.svg" alt="Clerkie Challenge" width={15} height={15} />
-        <h1 className="text-xs font-bold">Clerkie Challenge</h1>
-      </div>
       <NavLinks/>
     </div>
   );
